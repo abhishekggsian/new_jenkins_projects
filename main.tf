963ace372a82b03f9ef74b420df3cd43fc6bdc0a
abhishekggsian <157734536+abhishekggsian@users.noreply.github.com>
2025-06-25 13:26:26 +0530

    new (#1)
    merge

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "1.12.1"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3a734e32-021d-4243-89ff-c3495e6aa4da"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-abhi-001"
+  location = "westus"
+}
